04a95324e8ec5d348d7ef2ebb797bd2c3f01da9f
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-11-25 17:35:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NetFrameworkSample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Name = "John Doe",
+                Email = "john@nuget.org",
+                DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
